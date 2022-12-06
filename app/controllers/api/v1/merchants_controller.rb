9725f5824fdd728b9e01704fb72f97f746edfc53
Joseph Hilby <joseph.hilby@gmail.com>
2022-12-06 16:00:56 -0600

    Add errors to merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::MerchantsController < ApplicationController
+
   def index
-    render json: Merchant.all
+    render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show
-    render json: Merchant.find(params[:id])
+    if Merchant.exists?(params[:id])
+      render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    else
+      render json: { errors: 'Not Found' }, status: 404
+    end
   end
 end
