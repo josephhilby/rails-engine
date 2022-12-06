3c274adc4358710159545eff0860c2703e81a7f6
Joseph Hilby <joseph.hilby@gmail.com>
2022-12-06 15:58:54 -0600

    Add tests for merchant serializer

--- spec/requests/api/v1/merchants/index_spec.rb
+++ spec/requests/api/v1/merchants/index_spec.rb
@@ -1,29 +1,22 @@
 require 'rails_helper'
 
 describe "Index Merchants API" do
-  it "can GET a list of merchants" do
+  it "can GET all merchants" do
     create_list(:merchant, 3)
-
     get api_v1_merchants_path
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(merchants.count).to eq(3)
 
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_an(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
-
-      expect(merchant).to have_key(:created_at)
-      expect(merchant[:created_at]).to be_a(String)
-
-      expect(merchant).to have_key(:updated_at)
-      expect(merchant[:updated_at]).to be_an(String)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 end
--- spec/requests/api/v1/merchants/show_spec.rb
+++ spec/requests/api/v1/merchants/show_spec.rb
@@ -1,25 +1,36 @@
 require 'rails_helper'
 
 describe "Index Merchants API" do
-  it "can GET one merchant by ID" do
-    id = create(:merchant)
+  context 'given a valid ID' do
+    it "can GET one merchant by ID" do
+      id = create(:merchant)
+      get api_v1_merchant_path(id)
 
-    get api_v1_merchant_path(id)
+      expect(response).to be_successful
 
-    expect(response).to be_successful
+      merchant = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    merchant = JSON.parse(response.body, symbolize_names: true)
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(String)
 
-    expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
 
-    expect(merchant).to have_key(:name)
-    expect(merchant[:name]).to be_a(String)
+  context 'given a non-valid ID' do
+    it 'returns an error' do
+      10.times { create(:merchant) }
+      get api_v1_merchant_path(Merchant.last.id + 1)
 
-    expect(merchant).to have_key(:created_at)
-    expect(merchant[:created_at]).to be_a(String)
+      expect(response).not_to be_successful
 
-    expect(merchant).to have_key(:updated_at)
-    expect(merchant[:updated_at]).to be_an(String)
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(404)
+
+      expect(merchant).to have_key(:errors)
+      expect(merchant[:errors]).to be_a(String)
+    end
   end
 end
