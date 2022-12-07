111f76df10e4358211f921e3cd92e8e64f5f1e0f
Joseph Hilby <joseph.hilby@gmail.com>
2022-12-07 10:58:04 -0600

    Add routes and controller files

---
+++ app/controllers/api/v1/merchant_find_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::MerchantFindController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
       end
       resources :items
       get '/items/:id/merchant', to: 'item_merchants#show'
+
+      get '/items/find', to: 'item_find#show'
+      get '/items/find_all', to: 'item_find#index'
+
+      get '/merchants/find', to: 'merchant_find#show'
+      get '/merchants/find_all', to: 'merchant_find#index'
     end
   end
 end
