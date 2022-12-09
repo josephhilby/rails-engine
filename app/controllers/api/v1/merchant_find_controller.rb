2b847eb6ffafcf9cf825ecd4a6913d98fc74905e
Joseph Hilby <joseph.hilby@gmail.com>
2022-12-09 05:11:18 -0600

    Add merchant_find controller and tests

--- app/controllers/api/v1/merchant_find_controller.rb
+++ app/controllers/api/v1/merchant_find_controller.rb
@@ -1,10 +1,20 @@
 class Api::V1::MerchantFindController < ApplicationController
+  before_action :check_params
+
   def index
-    # require'pry';binding.pry
-    render json: MerchantSerializer.new(Merchant.where("name ~* :name", { :name => params[:name] }))
-    # render json: MerchantSerializer.new(Merchant.where("name ~* ?", params[:name]))
+    render json: MerchantSerializer.new(Merchant.where("name ~* :name", { :name => params[:name] }).order(:name))
   end
 
   def show
+    merchant = Merchant.where("name ~* :name", { :name => params[:name] }).order(:name).first || Merchant.new
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+
+  def check_params
+    if params[:name] == '' || !params[:name]
+      render json: { errors: 'Not Found' }, status: 400
+    end
   end
 end
--- spec/requests/api/v1/merchants/find/index_spec.rb
+++ spec/requests/api/v1/merchants/find/index_spec.rb
@@ -1,27 +1,73 @@
 require 'rails_helper'
 
 describe "Index Merchant_Find API" do
-  it "can GET all merchants that match given params" do
+  before(:each) do
     create(:merchant, name: "Johi")
     create(:merchant, name: "John Smith")
     create(:merchant, name: "Littlejohns Jacob")
     create(:merchant, name: "Mike Johnson")
-    get '/api/v1/merchants/find_all?name=john'
+  end
+  context 'given valid params' do
+    it "can GET all merchants that match given name" do
+      get '/api/v1/merchants/find_all?name=john'
 
-    # require'pry';binding.pry
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+      merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(merchants.count).to eq(3)
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an(String)
 
-    expect(merchants.count).to eq(3)
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+        expect(merchant[:attributes][:name].downcase.include?('john')).to be(true)
+      end
+    end
+  end
 
-    merchants.each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(String)
+  context 'given non-valid params' do
+    it 'returns an error' do
+      get '/api/v1/merchants/find_all?name=xkcd'
 
-      expect(merchant[:attributes]).to have_key(:name)
-      expect(merchant[:attributes][:name]).to be_a(String)
-      expect(merchant[:attributes][:name].downcase.include?('john')).to be(true)
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(response.status).to eq(200)
+
+      expect(merchant.length).to eq(0)
+      expect(merchant).to be_a(Array)
+    end
+  end
+
+  context 'given empity params' do
+    it 'returns an error' do
+      get '/api/v1/merchants/find_all?name='
+
+      expect(response).not_to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(400)
+
+      expect(merchant).to have_key(:errors)
+      expect(merchant[:errors]).to be_a(String)
+    end
+  end
+
+  context 'given no params' do
+    it 'returns an error' do
+      get '/api/v1/merchants/find_all'
+
+      expect(response).not_to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(400)
+
+      expect(merchant).to have_key(:errors)
+      expect(merchant[:errors]).to be_a(String)
     end
   end
 end
